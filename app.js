0a6670992966c6522c4a855762c981b263b50bd9
Peter Nilsson <peter@studiocherno.com>
2022-11-21 23:41:52 +0100

    Started redesign of website

---
+++ app.js
@@ -0,0 +1,33 @@
+// NodeJS server used for local testing of the site
+
+const fs = require('fs');
+const http = require('http');
+
+http.createServer((req, res) => {
+    if (req.url === '/')
+        req.url = '/index.html';
+
+    fs.readFile(__dirname + req.url, (err, data) => {
+        if (err)
+        {
+            res.writeHead(404);
+            
+            let response = JSON.stringify(err);
+
+            try {
+                response = fs.readFileSync(`${__dirname}/404.html`)
+            } catch(err2) {
+                console.log(err2);
+            }
+
+            res.end(response);
+        }
+        else
+        {
+            res.writeHead(200);
+            res.end(data);
+        }
+    });
+}).listen(8080, () => {
+    console.log(`Server listening on port 8080`);
+});
---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+function ReadTextFile(path, callback)
+{
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200)
+            callback(request.responseText);
+    };
+
+    request.open('GET', path, true);
+    request.send(null);
+}
+
+function GetFilesInFolder(path, callback)
+{
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200)
+            callback(request.responseText);
+    };
+
+    request.open('GET', path, true);
+    request.send(null);
+}
+
+window.addEventListener('load', () =>
+{
+    ReadTextFile('../includes/navbar.html', (text) =>
+    {
+        document.body.insertAdjacentHTML('afterbegin', text);
+    });
+
+    ReadTextFile('../includes/footer.html', (text) =>
+    {
+        document.body.insertAdjacentHTML('beforeend', text);
+    });
+
+    // let scrollPos = 0;
+    // const mainNav = document.getElementById('mainNav');
+    // const headerHeight = mainNav.clientHeight;
+    // window.addEventListener('scroll', () =>
+    // {
+    //     const currentTop = document.body.getBoundingClientRect().top * -1;
+
+    //     if ( currentTop < scrollPos)
+    //     {
+    //         // Scrolling Up
+    //     }
+    //     else
+    //     {
+    //         // Scrolling Down
+    //     }
+
+    //     scrollPos = currentTop;
+    // });
+});
