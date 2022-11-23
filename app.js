ea044fec6965bcd1c693c7d1313706376fc1e4e4
Peter Nilsson <peter@studiocherno.com>
2022-11-23 13:44:18 +0100

    Added initial blog post system

--- app.js
+++ app.js
@@ -1,36 +1,77 @@
 // NodeJS server used for local testing of the site
 
 const fs = require('fs');
-const http = require('http');
 const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const public = path.join(__dirname, 'public');
 
-http.createServer((req, res) => {
-    if (req.url === '/')
-        req.url = '/index.html';
+const app = express();
 
-    fs.readFile(path.join(public, req.url), (err, data) => {
+app.use(express.static(public));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('', (req, res) => res.sendFile(path.join(public, 'index.html')));
+app.get('/about', (req, res) => res.sendFile(path.join(public, 'about.html')));
+app.get('/post', (req, res) => res.sendFile(path.join(public, 'post.html')));
+app.get('/new-post', (req, res) => res.sendFile(path.join(__dirname, 'post-template.html')));
+
+app.post('/create-post', (req, res) =>
+{
+    // Check if post with that id already exists
+    let filePath = path.join(public, 'posts', `${req.body.postID}.json`);
+
+    if (fs.existsSync(filePath))
+    {
+        res.send(JSON.stringify({ responseCode: 1, message: `Post with ID ${req.body.postID} already exists.` }));
+        return;
+    }
+
+    fs.writeFile(filePath, JSON.stringify(req.body.postData), (err) =>
+    {
         if (err)
         {
-            res.writeHead(404);
-            
-            let response = JSON.stringify(err);
+            console.error(err);
+            res.send(JSON.stringify({ responseCode: 1, message: `Failed to write file to disk. ${err.message}` }));
+            return;
+        }
 
-            try {
-                response = fs.readFileSync(`${__dirname}/404.html`)
-            } catch(err2) {
-                console.log(err2);
+        let dbPath = path.join(public, 'posts', 'db.json');
+
+        fs.readFile(dbPath, (dbErr, data) =>
+        {
+            if (err)
+            {
+                res.send(JSON.stringify({ responseCode: 2, message: `Failed to read database. ${dbErr.message}` }));
+                return;
             }
 
-            res.end(response);
-        }
-        else
-        {
-            res.writeHead(200);
-            res.end(data);
-        }
+            let jsonData = JSON.parse(data);
+
+            // Remove last preview if there's already 5 posts in the array
+            if (jsonData.previews.length === 5)
+                jsonData.previews.pop();
+
+            jsonData.previews.unshift(req.body.postID);
+            jsonData.all.unshift(req.body.postID);
+
+            fs.writeFile(dbPath, JSON.stringify(jsonData), (dbWriteErr) =>
+            {
+                if (dbWriteErr)
+                {
+                    res.send(JSON.stringify({ responseCode: 1, message: `Failed to write database. ${dbWriteErr.message}` }));
+                    return;
+                }
+
+                res.send(JSON.stringify({ responseCode: 0 }));
+            });
+        });
     });
-}).listen(8080, () => {
+});
+
+app.listen(8080, () =>
+{
     console.log(`Server listening on port 8080`);
 });
